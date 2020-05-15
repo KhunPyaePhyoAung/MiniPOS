288afed2b407915d4f0b30979f5ee2fe94e04ba7
Khun Pyae Phyo Aung <khunpyaephyoaung74@gmail.com>
2020-05-15 13:17:19 +0630

    add Application class and Launcher class to launch it

---
+++ mini-pos/src/com/alphasfot/pos/Launcher.java
@@ -0,0 +1,7 @@
+package com.alphasfot.pos;
+
+public class Launcher {
+    public static void main(String[] args) {
+        PosApplication.main(args);
+    }
+}
---
+++ mini-pos/src/com/alphasfot/pos/PosApplication.java
@@ -0,0 +1,15 @@
+package com.alphasfot.pos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class PosApplication extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
