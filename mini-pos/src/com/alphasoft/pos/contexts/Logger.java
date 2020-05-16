776655be3ad89214d4aa883070a3585a4fd60544
Khun Pyae Phyo Aung <khunpyaephyoaung74@gmail.com>
2020-05-16 14:18:48 +0630

    add logger

---
+++ mini-pos/src/com/alphasoft/pos/contexts/Logger.java
@@ -0,0 +1,25 @@
+package com.alphasoft.pos.contexts;
+
+import com.alphasoft.pos.models.Account;
+
+public class Logger {
+    private static Logger logger;
+    private static Account loggedAccount;
+
+    private Logger(){
+
+    }
+
+    public void setLoggedAccount(Account account){
+        loggedAccount = account;
+    }
+
+    public Account getLoggedAccount(){
+        return loggedAccount;
+    }
+
+    public static Logger getLogger(){
+        if(null == logger) logger = new Logger();
+        return logger;
+    }
+}
