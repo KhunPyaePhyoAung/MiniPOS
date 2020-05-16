61b9d6975bbd6a0203efc88ebc89f4178dc770b0
Khun Pyae Phyo Aung <khunpyaephyoaung74@gmail.com>
2020-05-16 12:54:58 +0630

    add field and getter setter of Account

--- mini-pos/src/com/alphasoft/pos/models/Account.java
+++ mini-pos/src/com/alphasoft/pos/models/Account.java
@@ -1,4 +1,58 @@
 package com.alphasoft.pos.models;
 
 public class Account {
+    private int id;
+    private String loginId;
+    private String password;
+    private String name;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
