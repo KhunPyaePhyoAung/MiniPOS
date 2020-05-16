6c46b57bf0acd2b5312b74babd72808c65a0ddf7
Khun Pyae Phyo Aung <khunpyaephyoaung74@gmail.com>
2020-05-16 22:18:50 +0630

    completed loadView method in MainWindow

--- mini-pos/src/com/alphasoft/pos/views/controllers/MainWindowController.java
+++ mini-pos/src/com/alphasoft/pos/views/controllers/MainWindowController.java
@@ -2,9 +2,12 @@ package com.alphasoft.pos.views.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,18 +16,39 @@ import java.io.IOException;
 public class MainWindowController {
 
     @FXML
+    private HBox navigationBar;
+
+    @FXML
+    private StackPane contentPane;
+
+    @FXML
     public void requestView(MouseEvent event){
         var source = event.getSource();
         if(source instanceof VBox){
             VBox vBox = (VBox)source;
-            vBox.requestFocus();
+            loadView(vBox.getId());
         }
+    }
+
+    private void loadView(String viewName){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(String.format("/com/alphasoft/pos/views/%s.fxml",viewName)));
+            Parent view = fxmlLoader.load();
+            loadView(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadView(Parent view){
+        contentPane.getChildren().clear();
+        contentPane.getChildren().add(view);
+        navigationBar.getChildren().stream().filter(b->b.getId().equals(view.getId())).forEach(Node::requestFocus);
     }
 
     public static void show(){
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            Parent view = fxmlLoader.load(PosHomeController.class.getResource("/com/alphasoft/pos/views/main_window.fxml"));
+            Parent view = FXMLLoader.load(PosHomeController.class.getResource("/com/alphasoft/pos/views/main_window.fxml"));
             Stage stage = new Stage();
             stage.setScene(new Scene(view));
             stage.centerOnScreen();
