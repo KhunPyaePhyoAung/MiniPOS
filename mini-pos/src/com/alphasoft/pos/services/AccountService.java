4b77ddcc9a6582504ee09d4a094332d547bd4c7c
Khun Pyae Phyo Aung <khunpyaephyoaung74@gmail.com>
2020-05-16 13:55:23 +0630

    modified account service for loggin in

--- mini-pos/src/com/alphasoft/pos/services/AccountService.java
+++ mini-pos/src/com/alphasoft/pos/services/AccountService.java
@@ -1,6 +1,14 @@
 package com.alphasoft.pos.services;
 
+import com.alphasoft.pos.contexts.ConnectionManager;
 import com.alphasoft.pos.models.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.alphasoft.pos.contexts.SqlHelper.getQuery;
 
 public class AccountService {
 
@@ -11,15 +19,37 @@ public class AccountService {
     }
 
     public Account login(String loginId,String password){
-        Account account = new Account();
+
+        Account account = findAccountByLoginId(loginId);
+        
 
 
         return account;
     }
 
-    private Account findAccountByLoginId(){
-
+    private Account findAccountByLoginId(String loginId){
+        try(Connection connection = ConnectionManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(getQuery(""))
+        ) {
+            preparedStatement.setString(1,loginId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()){
+                return getAccount(resultSet);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return null;
+    }
+
+    private Account getAccount(ResultSet resultSet) throws SQLException {
+        Account account = new Account();
+        account.setLoginId(resultSet.getString("login_id"));
+        account.setPassword(resultSet.getString("password"));
+        account.setName(resultSet.getString("name"));
+        account.setPhone(resultSet.getString("phone"));
+        account.setEmail(resultSet.getString("email"));
+        return account;
     }
 
     public AccountService getService(){
