1a15c7ef2ec488f0a699e7014bc1d32f3a25d783
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2020-05-30 13:17:46 +0630

    fixed nav bar color effects

--- mini-pos/src/com/alphasoft/pos/views/controllers/MainWindowController.java
+++ mini-pos/src/com/alphasoft/pos/views/controllers/MainWindowController.java
@@ -64,6 +64,10 @@ public class MainWindowController implements Initializable {
         contentPane.getChildren().clear();
         contentPane.getChildren().add(view);
         navigationBar.getChildren().stream().filter(b->b.getId().equals(view.getId())).forEach(Node::requestFocus);
+        navigationBar.getChildren().stream().filter(b->b instanceof VBox).forEach(b->{
+                                                                                        b.getStyleClass().retainAll("navigation-icon-box");
+                                                                                        if(b.getId().equals(view.getId())) b.getStyleClass().add("navigation-icon-box-active");
+                                                                                    });
     }
 
     public static void show(){
